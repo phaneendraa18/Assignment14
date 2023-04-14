924171c50f6c0bddf954433792ed891fea38b878
phaneendraa18 <phaneendra.jonnala01@gmail.com>
2023-04-14 19:19:41 +0000

    initialcommit

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "google_compute_network" "vpc_network" {
+  project                 = "pj-project-374418"
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
+resource "google_compute_subnetwork" "test_subnetwork" {
+  project       = "pj-project-374418"
+  name          = "test-subnetwork"
+  network       = google_compute_network.vpc_network.name
+  ip_cidr_range = "10.0.0.0/22"
+  region        = "us-central1"
+}
+resource "google_compute_instance" "default" {
+  project      = "pj-project-374418"
+  count        = 2
+  name         = "tf-vm-${count.index}"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["fire1"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.test_subnetwork.name
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
